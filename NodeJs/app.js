b69b9db2c1914e0bbcb0d721d1b07c73b1ec6b3c
Kumar2 <RKumar2@ad.harman.com>
2018-04-19 18:42:53 +0530

    Post method for Protocol Api

--- NodeJs/app.js
+++ NodeJs/app.js
@@ -2,10 +2,17 @@ var express = require('express'),
     fs = require('fs'),
     xml2js = require('xml2js'),
     _ = require('lodash'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser')
+xmldom = require('xmldom');
+
+const ProtocolFilePath = '../automation/xml/options/protocol.xml';
+var parser = new xml2js.Parser(),
+    xmlBuilder = new xml2js.Builder(),
+    DOMParser = xmldom.DOMParser,
+    XMLSerializer = xmldom.XMLSerializer,
+    serializer = new XMLSerializer();;
 
 
-var parser = new xml2js.Parser();
 var app = express();
 
 var port = process.env.Port || 3000;
@@ -14,12 +21,40 @@ app.use(bodyParser.urlencoded({ extended: true }));
 var ProtocolRouter = express.Router();
 
 ProtocolRouter.route('/getProtocol')
-    .post(function (req, res) {
+    .put(function (req, res) {
+        fs.readFile(ProtocolFilePath, function (err, data) {
+            parser.parseString(data, function (err, result) {
+
+                console.log(result);
+
+                result.PROTOCOL.NAME = [req.body.name];
+
+                var xml = xmlBuilder.buildObject(result);
+
+                fs.writeFile(ProtocolFilePath, xml);
+
+            });
+        });
         console.log(req.body);
-        res.status(201).send(req.body);
+        res.status(201).send(req.body.name);
+    })
+    .post(function (req, res) {
+
+        var xmlString = fs.readFileSync(ProtocolFilePath, 'utf-8')
+        console.log(xmlString);
+        var doc;            
+        doc = new DOMParser().parseFromString(xmlString, 'application/xml');
+        var newEle = doc.createElement("NAME");
+        var newText = doc.createTextNode(req.body.name);
+        newEle.appendChild(newText);
+        doc.getElementsByTagName("PROTOCOL")[0].appendChild(newEle);
+
+        console.log(serializer.serializeToString(doc));
+        fs.writeFileSync(ProtocolFilePath, serializer.serializeToString(doc));           
+        res.status(201).send('Added Value:'+req.body.name);
     })
     .get(function (req, res) {
-        fs.readFile('../automation/xml/options/protocol.xml', function (err, data) {
+        fs.readFile(ProtocolFilePath, function (err, data) {
             parser.parseString(data, function (err, jresult) {
                 if (err) {
                     res.status(500).send(err);
@@ -33,7 +68,7 @@ ProtocolRouter.route('/getProtocol')
     });
 ProtocolRouter.route('/getProtocolByName/:name')
     .get(function (req, res) {
-        fs.readFile('../automation/xml/options/protocol.xml', function (err, data) {
+        fs.readFile(ProtocolFilePath, function (err, data) {
             parser.parseString(data, function (err, jresult) {
                 if (err) {
                     res.status(500).send(err);
