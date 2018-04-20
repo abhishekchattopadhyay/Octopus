50a0c459a96de979723baa99465b3ff81baec029
Kumar2 <RKumar2@ad.harman.com>
2018-04-20 17:12:14 +0530

    Protocol api completed with PUT GET POST DELETE

---
+++ NodeJs/api/protocol.js
@@ -0,0 +1,122 @@
+var express = require('express'),
+    fs = require('fs'),
+    xml2js = require('xml2js'),
+    _ = require('lodash'),
+    xmldom = require('xmldom'),
+    parser = new xml2js.Parser(),
+    xmlBuilder = new xml2js.Builder(),
+    DOMParser = xmldom.DOMParser,
+    XMLSerializer = xmldom.XMLSerializer,
+    serializer = new XMLSerializer(),
+    ProtocolRouter = express.Router();
+
+const ProtocolFilePath = '../automation/xml/options/protocol.xml';
+
+ProtocolRouter.route('/getProtocol')
+    .get(function (req, res) {
+        fs.readFile(ProtocolFilePath, function (err, data) {
+            parser.parseString(data, function (err, jresult) {
+                if (err) {
+                    res.status(500).send(err);
+                }
+                else {
+
+                    res.json(jresult.PROTOCOL.NAME);
+                }
+            });
+        });
+    })
+    .post(function (req, res) {
+        var xmlString = fs.readFileSync(ProtocolFilePath, 'utf-8');
+        console.log(xmlString);
+        var doc;
+        doc = new DOMParser().parseFromString(xmlString, 'application/xml');
+        var newEle = doc.createElement("NAME");
+        var newText = doc.createTextNode(req.body.name);
+        newEle.appendChild(newText);
+        doc.getElementsByTagName("PROTOCOL")[0].appendChild(newEle);
+        console.log(serializer.serializeToString(doc));
+        fs.writeFileSync(ProtocolFilePath, serializer.serializeToString(doc));
+        res.status(201).send('Added Value:' + req.body.name);
+    })
+    .put(function (req, res) {
+        var xmlString = fs.readFileSync(ProtocolFilePath, 'utf-8'),
+            doc,
+            flag = false,
+            Message,
+            doc = new DOMParser().parseFromString(xmlString, 'application/xml'),
+            Element = doc.getElementsByTagName('NAME'),
+            //New Element creation
+            NewElement = doc.createElement("NAME"),
+            newText = doc.createTextNode(req.body.newname);
+            NewElement.appendChild(newText);
+        for (let i = 0; i < Element.length; i++) {
+            if (serializer.serializeToString(Element[i].childNodes[0]) === req.body.oldname) {                      
+                doc.documentElement.replaceChild(NewElement,Element[i] );
+                flag = true;              
+            }
+        }
+        if (flag === true) {
+            fs.writeFileSync(ProtocolFilePath, serializer.serializeToString(doc));
+            console.log(serializer.serializeToString(doc));
+            Message = 'Item Upadted Success:' + req.body.newname;
+        }
+        else {
+            Message = 'No such item found in list:' + req.body.oldname;
+        }
+        //console.log(req.body.newname);
+        res.status(201).send(Message);
+    })
+    .delete(function (req, res) {
+        var xmlString = fs.readFileSync(ProtocolFilePath, 'utf-8'),
+            doc,
+            flag = false,
+            Message,
+            doc = new DOMParser().parseFromString(xmlString, 'application/xml'),
+            Element = doc.getElementsByTagName('NAME');
+        console.log(Element.length);
+        for (let i = 0; i < Element.length; i++) {
+            console.log(i);
+            console.log(serializer.serializeToString(Element[i]));
+            console.log(serializer.serializeToString(Element[i].childNodes[0]));
+            if (serializer.serializeToString(Element[i].childNodes[0]) === req.body.name) {
+                doc.documentElement.removeChild(Element[i]);
+                flag = true;
+            }
+        }
+        if (flag === true) {
+            fs.writeFileSync(ProtocolFilePath, serializer.serializeToString(doc));
+            console.log(serializer.serializeToString(doc));
+            Message = 'Item deteled Success:' + req.body.name;
+        }
+        else {
+            Message = 'No such item found in list:' + req.body.name;
+        }
+
+        res.status(201).send(Message);
+    });
+ProtocolRouter.route('/getProtocolByName/:name')
+    .get(function (req, res) {
+        fs.readFile(ProtocolFilePath, function (err, data) {
+            parser.parseString(data, function (err, jresult) {
+                if (err) {
+                    res.status(500).send(err);
+                }
+                else {
+                    var result;
+                    if (req.params.name) {
+                        result = _.filter(jresult.PROTOCOL.NAME, function (item) {
+                            return item == req.params.name;
+                        });
+                    }
+                    else {
+                        result = jresult.PROTOCOL.NAME;
+                    }
+
+                    res.json(result);
+                }
+            });
+        });
+    });
+
+module.exports = ProtocolRouter;
--- NodeJs/app.js
+++ NodeJs/app.js
@@ -1,100 +1,16 @@
 var express = require('express'),
-    fs = require('fs'),
-    xml2js = require('xml2js'),
-    _ = require('lodash'),
-    bodyParser = require('body-parser')
-xmldom = require('xmldom');
-
-const ProtocolFilePath = '../automation/xml/options/protocol.xml';
-var parser = new xml2js.Parser(),
-    xmlBuilder = new xml2js.Builder(),
-    DOMParser = xmldom.DOMParser,
-    XMLSerializer = xmldom.XMLSerializer,
-    serializer = new XMLSerializer();;
-
-
+bodyParser = require('body-parser');
 var app = express();
 
 var port = process.env.Port || 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-var ProtocolRouter = express.Router();
+var ProtocolRouter = require('./api/protocol');
 
-ProtocolRouter.route('/getProtocol')
-    .put(function (req, res) {
-        fs.readFile(ProtocolFilePath, function (err, data) {
-            parser.parseString(data, function (err, result) {
-
-                console.log(result);
-
-                result.PROTOCOL.NAME = [req.body.name];
-
-                var xml = xmlBuilder.buildObject(result);
-
-                fs.writeFile(ProtocolFilePath, xml);
-
-            });
-        });
-        console.log(req.body);
-        res.status(201).send(req.body.name);
-    })
-    .post(function (req, res) {
-
-        var xmlString = fs.readFileSync(ProtocolFilePath, 'utf-8')
-        console.log(xmlString);
-        var doc;            
-        doc = new DOMParser().parseFromString(xmlString, 'application/xml');
-        var newEle = doc.createElement("NAME");
-        var newText = doc.createTextNode(req.body.name);
-        newEle.appendChild(newText);
-        doc.getElementsByTagName("PROTOCOL")[0].appendChild(newEle);
-
-        console.log(serializer.serializeToString(doc));
-        fs.writeFileSync(ProtocolFilePath, serializer.serializeToString(doc));           
-        res.status(201).send('Added Value:'+req.body.name);
-    })
-    .get(function (req, res) {
-        fs.readFile(ProtocolFilePath, function (err, data) {
-            parser.parseString(data, function (err, jresult) {
-                if (err) {
-                    res.status(500).send(err);
-                }
-                else {
-
-                    res.json(jresult.PROTOCOL.NAME);
-                }
-            });
-        });
-    });
-ProtocolRouter.route('/getProtocolByName/:name')
-    .get(function (req, res) {
-        fs.readFile(ProtocolFilePath, function (err, data) {
-            parser.parseString(data, function (err, jresult) {
-                if (err) {
-                    res.status(500).send(err);
-                }
-                else {
-                    var result;
-                    if (req.params.name) {
-                        result = _.filter(jresult.PROTOCOL.NAME, function (item) {
-                            return item == req.params.name;
-                        });
-                    }
-                    else {
-                        result = jresult.PROTOCOL.NAME;
-                    }
-
-                    res.json(result);
-                }
-            });
-        });
-    });
-app.use('/api', ProtocolRouter);
-
+app.use('/api',ProtocolRouter);
 app.get('/', function (req, res) {
-    res.send('Welcome to My TestRunner Api');
+    res.send('Welcome to my TestRunner API');
 });
-
 app.listen(port, function () {
-    console.log('Server is on and running on Port number' + port);
+    console.log('Server is on and running on Port number:' + port);
 });
